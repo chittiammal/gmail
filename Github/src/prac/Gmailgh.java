31abe1e06ed79199f7e66a475ca7d74c47fc3c48
venkatesh <venkatesh@venkatesh-PC>
2018-07-13 11:12:47 +0530

    hai

--- Github/src/prac/Gmailgh.java
+++ Github/src/prac/Gmailgh.java
@@ -15,7 +15,6 @@ public class Gmailgh {
 		driver.findElement(By.xpath("//*[@id='identifierNext']")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//*[@name='password']")).sendKeys("shaibaba");
-		driver.findElement(By.id("passwordNext")).click();
 		Thread.sleep(2000);
 		driver.quit();
 	}
